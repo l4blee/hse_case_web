a9878c3e11d622037e87f1f4f281d0d21cd066ce
l4blee <l4blee@yandex.ru>
2022-11-02 18:13:15 +0900

    upd

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,20 +1,31 @@
 import classes from './Main.module.scss'
 
-import nstuLogo from '../../assets/NSTU_logo.png'
+import nstuLogo from '../../assets/NSTULogo.png'
+import teamLogo from '../../assets/teamLogo.png'
 
 
 export default function Main() {
     return (
         <div class={classes.Root}>
             <header class={classes.Header}>
-                <img class={classes.Logo} src={nstuLogo} alt='NSTU'/>
-                <h1>Коворкинг</h1>
+                {/* <img class={classes.Logo} src={nstuLogo} alt='NSTU'/> */}
+                
+                {/* <h1>Коворкинг</h1> */}
             </header>
-            <div class={classes.Container}>
-                <div class={classes.Info}>
-                    
+            <main class={classes.Body}>
+                <img class={classes.Team} src={teamLogo} alt='team'/>
+                <div class={classes.Container}>
+                    <div class={classes.block}>
+                        213
+                    </div>
+                    <div class={classes.block}>
+                        213
+                    </div>
+                    <div class={classes.block}>
+                        213
+                    </div>
                 </div>  
-            </div>
+            </main>
         </div>
     )
 }
